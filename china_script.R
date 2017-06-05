02889ba84323ee60aea49b73a852f7452691cecf
Joseph Carl <josephcarl7@gmail.com>
2017-06-04 19:30:33 -0700

    results of reduced model, lag order selection, IRFs

--- china_script.R
+++ china_script.R
@@ -11,6 +11,7 @@ library(readxl)
 library(tidyverse)
 library(urca)
 library(dynlm)
+library(stargazer)
 source("Functions_ECON_5305.R")
 
 # Read in co2 data
@@ -80,6 +81,10 @@ ts.plot(ChinaElec)
 g.elec <- diff(log(ChinaElec))
 ts.plot(g.elec)
 
+acf(g.elec)
+pacf(g.elec)
+acf.pacf(g.elec)
+
 ## Industry as % of GDP
 Industry <- read_excel("Industry (p of GDP).xlsx")
 Industry <- Industry %>% 
@@ -128,6 +133,49 @@ pacf(g.coal)
 acf.pacf(g.coal)
   # Maybe an ARMA(1,2)
 
+# % of Population Employed
+empdat <- read_excel("nationaldata.xls")
+tot.emp <- empdat %>% filter(Commodity_Desc == "Total employment") %>% 
+  arrange(Year_Desc) %>% 
+  mutate(Pop = Amount*10000/1000000) %>% 
+  select(Pop) %>% ts(., start=c(1952,1), frequency=1)
+tot.pop <- empdat %>% filter(Commodity_Desc == "Population") %>% 
+  arrange(Year_Desc) %>% 
+  mutate(Pop = Amount*10000/1000000) %>% 
+  select(Pop) %>% ts(., start=c(1952,1), frequency=1)
+perc.employed <- tot.emp/tot.pop
+
+ts.plot(perc.employed)
+
+g.perc.employed <- diff(log(perc.employed))
+
+ts.plot(g.perc.employed)
+
+acf(g.perc.employed)
+pacf(g.perc.employed)
+acf.pacf(g.perc.employed)
+
+perc.unemp <- 1-perc.employed
+ts.plot(perc.unemp)
+
+g.perc.unemp <- diff(log(perc.unemp))
+acf(g.perc.unemp)
+pacf(g.perc.unemp)
+acf.pacf(g.perc.unemp)
+
+# Exchange rate
+ex.rate <- empdat %>% filter(Commodity_Desc == "Exchange rate") %>% 
+  arrange(Year_Desc) %>% select(Amount) %>% 
+  ts(., start=c(1960,1), frequency=1)
+
+ts.plot(ex.rate)
+
+g.ex.rate <- diff(log(ex.rate))
+
+ts.plot(g.ex.rate)
+
+
+
 ##### Unit Root Tests #####
 adf.results(China.co2, max.lags = 1)
 adf.results(g.China, max.lags = 1)
@@ -141,7 +189,11 @@ adf.results(ChinaElec, max.lags = 1)
   # China Electricity consumption per capita is nonstationary
 adf.results(g.elec, max.lags = 1)
   # Even the growth rates of electricity consumption per capita are nonstationary
-  # 1st-diff the series and try again
+g.elec.test.drift <- ur.df(g.elec, type = "drift", lags = 1)
+summary(g.elec.test.drift)
+g.elec.test.trend <- ur.df(g.elec, type = "trend", lags = 1)
+summary(g.elec.test.trend)
+# 1st-diff the series and try again
 g.g.elec <- diff(g.elec)
 adf.results(g.g.elec, max.lags = 1)
   # Now the series is stationary
@@ -162,39 +214,51 @@ adf.results(ChinaCoal, max.lags = 1)
 adf.results(g.coal, max.lags = 1)
   # growth rate of coal consumption per capita is stationary
 
+adf.results(perc.employed, max.lags = 1)
+adf.results(g.perc.employed, max.lags = 1)
+
+adf.results(perc.unemp, max.lags = 1)
+adf.results(g.perc.unemp, max.lags = 1)
+
+adf.results(ex.rate, max.lags = 1)
+adf.results(g.ex.rate, max.lags = 1)
+
 
 ##### VAR Model ######
 
-# 3 variables: g.China (CO2), ChinaGDPgrowth, g.coal
+# 3 variables: g.China (CO2), ChinaGDPgrowth, g.elec
 
-z <- cbind(g.China, ChinaGDPgrowth, g.coal) %>% na.omit
+z <- cbind(g.China, ChinaGDPgrowth, g.elec) %>% na.omit
 cov.matrix <- var(z) # Reduced form covariance matrix
 
 # Estimate the models
-  # Will use 1 lag for all models because each variable exhibited AR(1) behavior
+  # Will use 1 lag because VARselect() said minimum AIC occurs when using 1 lag
+  # and theoretically it makes sense that what happened 2+ years ago has little explanatory power
+  # for what happens today
 
-eq1 <- dynlm(g.China ~ L(g.China, 1) + L(ChinaGDPgrowth, 1) + L(g.coal, 1) + 
-               L(g.China, 2) + L(ChinaGDPgrowth, 2) + L(g.coal, 2), data = z)
-eq2 <- dynlm(ChinaGDPgrowth ~ L(g.China, 1) + L(ChinaGDPgrowth, 1) + L(g.coal, 1) + 
-               L(g.China, 2) + L(ChinaGDPgrowth, 2) + L(g.coal, 2), data = z)
-eq3 <- dynlm(g.coal ~ L(g.China, 1) + L(ChinaGDPgrowth, 1) + L(g.coal, 1) + 
-               L(g.China, 2) + L(ChinaGDPgrowth, 2) + L(g.coal, 2), data = z)
+eq1 <- dynlm(g.China ~ L(g.China, 1) + L(ChinaGDPgrowth, 1) + L(g.elec, 1), data = z)
+eq2 <- dynlm(ChinaGDPgrowth ~ L(g.China, 1) + L(ChinaGDPgrowth, 1) + L(g.elec, 1), data = z)
+eq3 <- dynlm(g.elec ~ L(g.China, 1) + L(ChinaGDPgrowth, 1) + L(g.elec, 1), data = z)
+
+# Nice output (Stargazer) of 3 equations
+stargazer(eq1, eq2, eq3, type = "text", covariate.labels = c("lag CO2", "lag GDP", "lag Electricity"),
+          dep.var.labels = c("CO2", "GDP", "Electricity"))
+
 
 # Extract coefficients to a matrix
 coef.mat <- local({
   out <- rbind(coef(eq1), coef(eq2), coef(eq3))
-  rownames(out) <- c("eq.CO2", "eq.GDP", "eq.COAL")
+  rownames(out) <- c("eq.CO2", "eq.GDP", "eq.elec")
   out
 })
 Dhat.0 <- coef.mat[,1, drop=FALSE]        # matrix of constants
 Dhat.1 <- coef.mat[,2:4, drop=FALSE]      # matrix of coefficeints on Y_{t-1} and X_{t-1}
-Dhat.2 <- coef.mat[,5:7, drop=FALSE]      # matrix of coefficeints on Y_{t-2} and X_{t-2}
 
 # Get fitted residuals
 ehat.CO2 <- resid(eq1)
 ehat.GDP <- resid(eq2)
-ehat.coal <- resid(eq3)
-ehat <- rbind(ehat.CO2, ehat.GDP, ehat.coal)
+ehat.elec <- resid(eq3)
+ehat <- rbind(ehat.CO2, ehat.GDP, ehat.elec)
 
 sigma.ehat <- var(t(ehat))
 
@@ -210,16 +274,17 @@ solve(B)  # B-inverse
 A0 <- (-1)*(solve(B) - diag(3))   # A0 matrix
 Gamma.0 <- solve(B) %*% Dhat.0 
 Gamma.1 <- solve(B) %*% Dhat.1
-Gamma.2 <- solve(B) %*% Dhat.2
 
 # View the recovered structural parameters
-Gamma.0; Gamma.1; Gamma.2
+Gamma.0; Gamma.1
 
 # Compare to textbook commands
-var.mod <- vars::VAR(z, ic = c("AIC"), lag.max=4)
+var.mod <- vars::VAR(z, ic = c("AIC"), lag.max=6)
 summary(var.mod)
 
 var.1lag <- vars::VAR(z, p=1)
+summary(var.1lag)
+
 logLik(var.1lag)
 var.2lag <- vars::VAR(z, p=2)
 summary(var.2lag)
@@ -230,8 +295,61 @@ pchisq(LRtest.stat, df = 12, lower.tail = F)
   # the likelihood ratio test says that model with 2 lags is better fit than 1 lag
 
 # Select max number of lags
-vars::VARselect(z, lag.max = 8, type = "const")
+vars::VARselect(z, lag.max = 8, type = "trend")
   # This method agrees that 1 lag is the best choice for the VAR model
 
 test.mod <- dynlm(g.China ~ L(g.China, 1) + L(ChinaGDPgrowth, 1) + L(g.coal, 1) + L(g.g.elec) + L(g.ind,1))
 summary(test.mod)
+
+
+
+###### IRF #######
+# Impulse response function for a 1-std dev shock
+
+# Dr. Bejan's IRF function
+irf.fun <- function(A.mat, C.mat, struct.shock, n.ahead){
+  #---------------------------------------------------------------#
+  #-- Inputs:                                                   --#
+  #--     A.mat: 2-by-2 matrix of 1st lag coefficients          --#
+  #--     C.mat: Choleski decomposition matrix                  --#
+  #--     n.ahead: length of the IRFs (periods after the shock) --#
+  #--     struct.shock: (0,1) vector of structural shocks       --#
+  #---------------------------------------------------------------#
+  shocks <- matrix(struct.shock, ncol=1)                        
+  df <- matrix(0, ncol=n.ahead, nrow=nrow(A.mat))
+  for (i in 1:n.ahead){
+    if (i == 1){
+      df[,i] <- C.mat %*% shocks
+    } else {
+      df[,i] <- A.mat %*%  df[,i-1]
+    }
+  }  
+  rownames(df) <- rownames(A.mat)
+  colnames(df) <- paste("irf.",0:(n.ahead-1),sep="")
+  out <- t(df)  # transpose (time: by row, variable: by column)
+}
+co2.shock.irf <- irf.fun(A.mat = Dhat.1, C.mat = B, struct.shock = c(1,0,0), n.ahead = 12)
+gdp.shock.irf <- irf.fun(A.mat = Dhat.1, C.mat = B, struct.shock = c(0,1,0), n.ahead = 12)
+elec.shock.irf <- irf.fun(A.mat = Dhat.1, C.mat = B, struct.shock = c(0,0,1), n.ahead = 12)
+
+local({
+  par(mar = rep(2, 4))
+  par(mfrow=c(3,3))    # place 4 plots on 1 page (2-by-2)
+  ts.plot(co2.shock.irf[,"eq.CO2"], ylab ="CO2", main="CO2 --> CO2")
+  ts.plot(co2.shock.irf[,"eq.GDP"], ylab ="GDP", main="CO2 --> GDP")
+  ts.plot(co2.shock.irf[,"eq.elec"], ylab ="Electricity", main="CO2 --> Elec")
+  
+  ts.plot(gdp.shock.irf[,"eq.CO2"], ylab ="CO2", main="GDP --> CO2")
+  ts.plot(gdp.shock.irf[,"eq.GDP"], ylab ="GDP", main="GDP --> GDP")
+  ts.plot(gdp.shock.irf[,"eq.elec"], ylab ="Elec", main="GDP --> Elec")
+  
+  ts.plot(elec.shock.irf[,"eq.CO2"], ylab ="CO2", main="Elec --> CO2")
+  ts.plot(elec.shock.irf[,"eq.GDP"], ylab ="GDP", main="Elec --> GDP")
+  ts.plot(elec.shock.irf[,"eq.elec"], ylab ="x", main="Elec --> Elec")
+  par(mfrow=c(1,1))    # place 1 plot per page
+})
+
+var1.reorder <- vars::VAR(z[,c("g.elec", "ChinaGDPgrowth", "g.China")], p = 1)
+irf.noconf <- vars::irf(var1.reorder, n.ahead=12, boot=F)
+plot(irf.noconf)
+
